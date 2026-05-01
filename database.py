d4d3f07090a42702c694019c8b33300e84cb0444
NGiri11 <navneetgiri2004@gmail.com>
2026-05-01 18:13:24 +0530

    deployment setup

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "mysql+mysqlconnector://root:Ngiri1174%40@127.0.0.1/team_task_manager"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
