4cf8d41d5a0ef38b9556b6cb7840c6290ae8ea6a
NGiri11 <navneetgiri2004@gmail.com>
2026-05-01 18:27:07 +0530

    fix railway mysql url

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL.startswith("mysql://"):
+    DATABASE_URL = DATABASE_URL.replace("mysql://", "mysql+mysqlconnector://")
 
 engine = create_engine(DATABASE_URL)
 
